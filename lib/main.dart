ed46f8c104f56f892d60ac96ef69abae59217da8
Anuoluwapo Famakinwa <famakinwamercy@gmail.com>
2022-06-15 10:44:11 -0700

    onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mind_pal/screens/onboarding.dart';
 import 'package:mind_pal/screens/splashscreen.dart';
 
 void main() {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const MaterialApp(debugShowCheckedModeBanner: false,
       title: 'Mind Pal',
       home: Splashscreen(),
     );
--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
+import 'onboarding.dart';
 
 class Splashscreen extends StatefulWidget {
   const Splashscreen({Key? key}) : super(key: key);
@@ -9,7 +13,18 @@ class Splashscreen extends StatefulWidget {
 
 class _SplashscreenState extends State<Splashscreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context)=>Onboarding()));
+    });
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Color(0xffFFDAB9),
+      body: Center(child: Image.asset("assets/images/splash.png",),)
+    );
   }
 }
